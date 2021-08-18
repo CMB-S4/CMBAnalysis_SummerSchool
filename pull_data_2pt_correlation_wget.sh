d5871809b608addd60b5992442cd95fb5ac8fbba
Renee Hlozek <renee.hlozek@gmail.com>
2021-08-17 21:46:41 -0400

    updated sh file to pull SDSS data

--- pull_data_2pt_correlation_wget.sh
+++ pull_data_2pt_correlation_wget.sh
@@ -2,5 +2,9 @@
 
 # ~~~~~~~ 2pt-correlation ~~~~~~~~
 
-wget -O SDSS_spextral_galaxies_z0.1to0.2_wide.csv http://www.dunlap.utoronto.ca/~hlozek/CMBS4School/SDSS_spextral_galaxies_z0.1to0.2_wide.csv
+wget -O SDSS_spextral_galaxies_z0.1to0.2_wide.csv https://www.dropbox.com/s/au4c09gluh16ua0/SDSS_spextral_galaxies_z0.1to0.2_wide.csv?dl=0
+wget -O SDSS_spextral_galaxies_z0.4to0.5_wide.csv https://www.dropbox.com/s/otkcdjzrds1zmxq/SDSS_spextral_galaxies_z0.4to0.5_wide.csv?dl=0
+wget -O SDSS_spextral_galaxies_z0.6to0.9_wide.csv https://www.dropbox.com/s/1naejtptbdhjsot/SDSS_spextral_galaxies_z0.6to0.9_wide.csv?dl=0
+
+
 
